431ef29ceb968f1e36dcb8cd78cc0f39ad869bc3
Pehnny <theo.piron.pro@gmail.com>
2025-01-14 13:46:48 +0100

    Added(Delete operation)

--- delete.php
+++ delete.php
@@ -1,3 +1,12 @@
 <?php
-/**** Supprimer une randonnée ****/
+    header('Location: http://localhost:5000/read.php');
+
+    try {
+        include 'database/connect.php';
+        $connexion = connectToDatabase();
+        $query = $connexion->query('DELETE FROM '.getTableName()." WHERE id = '{$_GET['id']}'");
+        exit(0);
+    } catch(PDOException $exception) {
+        exit(1);
+    }
 ?>
